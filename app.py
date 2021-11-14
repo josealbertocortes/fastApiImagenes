40127e33525808fbe01d77a16a3f0c4bb1ecbf15
josealbertocortes <josealbertocortesayala94@gmail.com>
2021-11-13 22:27:00 -0500

    server static files

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI,UploadFile,File
 from fastapi.datastructures import UploadFile
 from config.db import client
 from os import getcwd,path,mkdir
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="./imagenes"), name="static")
+
 @app.post('/imagenes')
 async def upload_imagen(file:UploadFile=File(...)):
     if path.exists(getcwd()+'/imagenes/'):
